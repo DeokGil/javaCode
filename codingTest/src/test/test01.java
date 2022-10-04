1041316bfaa4cbcf57e56a9aa6dd534cd5124f2a
DeokGil <hstmambo93@naver.com>
2022-10-04 23:03:37 +0900

    프로그래머스 java-1

---
+++ codingTest/src/test/test01.java
@@ -0,0 +1,23 @@
+package test;
+
+public class test01 {
+//정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+	
+
+ // num은 int 범위의 정수입니다.
+//	0은 짝수입니다
+	
+// 홀수 :Odd , 짝수: Even 
+	
+	public String solution(int num) {
+		String answer ="";
+		if(num % 2 == 0) {
+			answer ="Even";
+		}else {
+			answer = "Odd";
+		}
+		return answer;
+	}
+}
+
+
